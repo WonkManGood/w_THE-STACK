053e334cf212c971de397521e3e91203e05833b4
WonkManGood <ezrajwadding@gmail.com>
2025-01-24 13:48:01 -0500

    'Commited via script.'

---
+++ stack/pins.py
@@ -0,0 +1,29 @@
+from machine import(
+    Pin,
+    PWM,
+)
+
+## pico pins
+GPIO0 = ...
+GPIO1 = ...
+GPIO2 = PWM(Pin(2, Pin.OUT), freq=100, duty_ns=100) # purplegp
+GPIO4 = PWM(Pin(4, Pin.OUT), freq=100, duty_ns=100) # purple
+GPIO5
+GPIO6
+GPIO7
+GPIO8
+GPIO9
+GPIO10
+GPIO11
+GPIO12
+GPIO13
+GPIO14
+GPIO15
+GPIO16
+GPIO17
+GPIO18
+GPIO19
+GPIO20
+GPIO21
+GPIO22
+GPIO_LED
